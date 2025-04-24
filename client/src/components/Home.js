4fc779f9657e207e5826f50ae5380df842b7ce6b
João Pedro Gonçalves <joao.araujo@sou.inteli.edu.br>
2025-04-24 12:29:42 -0300

    FEAT/FIX: Adicionando vídeo e corrigindo responsividade

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,94 +1,106 @@
 import React from 'react';
 import logo from '../imgs/logo.svg';
 import faixa from '../imgs/faixa.png';
+import fundoVideo from '../videos/fundo-futebol.mp4';
 import { Routes, Route, Link } from 'react-router-dom';
 
 const Home = () => {
     return (
+
         <div class="Home">
-            <div className="menu-container">
-                <div className="logo-container">
-                    <Link to="/">
-                        <img src={logo} alt="Logo" className="logo" />
-                    </Link>
-                </div>
-                <center>
-                    <div className="logo-container2">
-                        <Link className="no-effect" to="/docs">Docs</Link>
-                        <Link className="no-effect" to="/contacts">Contact</Link>
-                        <Link className="no-effect" to="/dapps">Dapps</Link>
-                        <Link className="no-effect" to="/roadmap">Roadmap</Link>
+            <div class='container-video'>
+                <video className="bg-video" autoPlay muted loop playsInline>
+                    <source src={fundoVideo} type="video/mp4" />
+                </video>
+                <div className="video-overlay"></div>
+                <div className="menu-container">
+                    <div className="logo-container">
+                        <Link to="/">
+                            <img src={logo} alt="Logo" className="logo" />
+                        </Link>
                     </div>
-                </center>
-                <div className="profile-container">
-                </div>
-            </div>
-            <div class="content-container">
-                <div class="text-content">
-                    <h1 class="title">Sportiverse is here!</h1>
-                    <p class="subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    <div class="button-group">
-                        <button class="btn blue">Start</button>
-                        <button class="btn gray">Learn more</button>
+                    <center>
+                        <div className="logo-container2">
+                            <Link className="no-effect actual" to="/">Home</Link>
+                            <Link className="no-effect" to="/docs">Docs</Link>
+                            <Link className="no-effect" to="/contacts">Contact</Link>
+                            <Link className="no-effect" to="/dapps">Dapps</Link>
+                        </div>
+                    </center>
+                    <div className="profile-container">
                     </div>
                 </div>
-            </div>
+                <div class="content-container">
 
-            <div class="section-wrapper spacing-xl">
-                <div class="section section-left">
-                    <h1 class="section-title">Blockchain that Scale</h1>
-                    <p class="section-subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    <button class="btn blue">Learn about AppChains</button>
+                    <div class="text-content">
+                        <h1 class="title">Sportiverse is here!</h1>
+                        <p class="subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        <div class="button-group">
+                            <button class="btn blue">Start</button>
+                            <button class="btn gray">Learn more</button>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <img class='faixa' src={faixa} />
+            <div class="section-general">
+                <div class="section-wrapper spacing-xl">
+                    <div class="section section-left">
+                        <h1 class="section-title">Blockchain that Scale</h1>
+                        <p class="section-subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        <button class="btn blue">Learn about AppChains</button>
+                    </div>
+                </div>
 
-            <div class="section-wrapper spacing-xl">
-                <div class="section section-right">
-                    <h1 class="section-title">Blockchain that Scale</h1>
-                    <p class="section-subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    <button class="btn blue">Learn about AppChains</button>
+                <div class="section-wrapper spacing-xt">
+                    <div class="section section-right">
+                        <h1 class="section-title">Blockchain that Scale</h1>
+                        <p class="section-subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        <button class="btn blue">Learn about AppChains</button>
+                    </div>
                 </div>
             </div>
+            <div class="section-design">
+                <div class="section-wrapper spacing-lg">
+                    <div class="section section-right">
+                        <h1 class="section-title2">Design to spec, at internet scale</h1>
+                        <p class="section-subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        <button class="btn white">Learn about AppChains</button>
+                    </div>
+                </div>
 
-            <div class="section-wrapper spacing-lg">
-                <div class="section section-right">
-                    <h1 class="section-title2">Design to spec, at internet scale</h1>
-                    <p class="section-subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    <button class="btn white">Learn about AppChains</button>
+                <div class="section-wrapper spacing-right">
+                    <div class="section section-right">
+                        <p class="section-subtitle centered">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
+                    </div>
                 </div>
-            </div>
-
-            <div class="section-wrapper">
-                <div class="section section-right">
-                    <p class="section-subtitle centered">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
+                <div className="section-wrapper spacing-right">
+                    <div className="section section-right">
+                        <center>
+                            <p className="section-subtitle">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
+                        </center>
+                    </div>
                 </div>
-            </div>
-            <div className="section-wrapper">
-                <div className="section section-right">
-                    <center>
-                        <p className="section-subtitle">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
-                    </center>
+                <div className="section-wrapper spacing-right">
+                    <div className="section section-right">
+                        <center>
+                            <p className="section-subtitle">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
+                        </center>
+                    </div>
                 </div>
-            </div>
-            <div className="section-wrapper">
-                <div className="section section-right">
-                    <center>
-                        <p className="section-subtitle">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
-                    </center>
+                <div className="section-wrapper spacing-right">
+                    <div className="section section-right">
+                        <center>
+                            <p className="section-subtitle">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
+                        </center>
+                    </div>
                 </div>
-            </div>
-            <div className="section-wrapper">
-                <div className="section section-right">
-                    <center>
-                        <p className="section-subtitle">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
-                    </center>
-                </div>
-            </div>
-            <div className="section-wrapper">
-                <div className="section section-right">
-                    <center>
-                        <p className="section-subtitle">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
-                    </center>
+                <div className="section-wrapper spacing-right">
+                    <div className="section section-right">
+                        <center>
+                            <p className="section-subtitle">+ Build with full sovereignty, from choice of validators to customizing your runtime.</p>
+                        </center>
+                    </div>
                 </div>
             </div>
             <div className="card-section">
@@ -105,10 +117,11 @@ const Home = () => {
                     <p className="card-text">Seamlessly connect with multiple blockchains.</p>
                 </div>
             </div>
+
             <footer className="footer">
                 <p>Created by Sportiverse Labs © {new Date().getFullYear()}</p>
             </footer>
-        </div >
+        </div>
     );
 };
 
