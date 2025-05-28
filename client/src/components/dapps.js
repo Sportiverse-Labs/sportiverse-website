54788eb62d6682cadf2cae3af8e7af819f6566c2
João Pedro Gonçalves <joao.araujo@sou.inteli.edu.br>
2025-05-27 21:52:18 -0300

    Update dapps.js

--- client/src/components/dapps.js
+++ client/src/components/dapps.js
@@ -88,7 +88,7 @@ const Dapps = () => {
                     <div class="section section-left">
                         <h1 class="section-title dap">Your first social media on WEB3!</h1>
                         <p class="subtitle dap">See another Players, receive bids, watch another matches and see the trendings.</p>
-                        <a href="https://tutorials.cosmos.network/" className="no-effect">
+                        <a href="" className="no-effect">
                             <button class="btn blue">See the DAPP</button>
                         </a>
                     </div>
